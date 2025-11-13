03e915a1a3bbb6f334d6cab6b6ac7a02ab4af529
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-11-13 14:13:35 +0300

    feat: Enhance API schema view with permission and authentication classes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,10 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+
 
 schema_view = get_schema_view(
     openapi.Info(title="Flawel Api", default_version='v1'),
     public=True,
+    permission_classes=(permissions.AllowAny,),
+    authentication_classes=[SessionAuthentication, BasicAuthentication],
 )
 
 urlpatterns = [
