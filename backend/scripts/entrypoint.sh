8402c51afe57d731a10ca885125c698b09d6c6f6
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2026-08-08 12:46:10 +0300

    fix: add --noinput flag to migrate command and ensure script execution stops on error

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 python manage.py collectstatic --noinput
-python manage.py migrate
-exec "$@"
+python manage.py migrate --noinput
+exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
